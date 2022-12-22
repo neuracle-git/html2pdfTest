1ae338db54df9ed15ee901a02d9e459ccdc1d56b
Neuracle Technologies <keshav@neuracle.in>
2022-12-22 11:09:10 +0530

    css updates, url split name

--- app.js
+++ app.js
@@ -32,16 +32,19 @@ async function printPDF() {
 
 // await page.setViewport({ width: 0, height: 0});
 
-  await page.goto('https://www.iwant2explore.com/agra-uttar-pradesh-2-nights-3-days-itinerary', {waitUntil: 'networkidle0'});
+  var url = 'https://www.iwant2explore.com/prayagraj-1-night-2-days-allahabad-itinerary';
+
+  await page.goto(url, {waitUntil: 'networkidle0'});
   
-  
+  const url_sct = url.split("/").pop();
+
   // await page.waitForSelector('div > .grecaptcha-badge');
   await page.waitForSelector('.det');
   // Query for an element handle.
 
-  await page.evaluate(()=>{
-    document.body.innerHTML += '<div class="watermark">Visa 2 Explore</div>';
-})
+//   await page.evaluate(()=>{
+//     document.body.innerHTML += '<div class="watermark">Visa 2 Explore</div>';
+// })
 
   const elHandleArray = await page.$$('summary');
 
@@ -65,7 +68,7 @@ async function printPDF() {
 
     //  content: '.watermark{position: absolute;top: 0;left: 0;bottom: 0;z-index: -1;transform: rotate(300deg);-webkit-transform: rotate(300deg);color: #c6afaf;}  .activity .row div{padding-right:0 !important;padding-left:0 !important} .quickinfoSection .row .col-12{width:25% !important} .quickinfoSection .row .col-sm-6{width:25% !important} ::marker{color:black !important} .col-lg-8{width:100%} .content-section .section .container-fluid .container{margin:0;max-width:-webkit-fill-available} .contentblock{break-inside:avoid} .overviewContent{break-inside:avoid} .day{break-inside:avoid} .activity{border:solid;border-width:thin !important; margin-top:20px !important;margin-bottom:30px !important;break-inside:avoid} .styles_scroll-to-top__2A70v {display:none} #headerId { display: none} .collapseContent { max-height: 100% !important} .readmore{display:none} .engagementSection{display:none} .widget{display:none} .footer{display:none} .footer-subscribe{display:none} .grecaptcha-badge{display:none !important;} form{display:none}' 
 
-  const pdf = await page.pdf({ path: 'tr_v52.pdf', 
+  const pdf = await page.pdf({ path: url_sct+'.pdf', 
                                 format: 'A4',
                                 // displayHeaderFooter: true,
                                 // headerTemplate: '<span style="font-size: 20px; width: 200px; height: 20px; color: black;">Visa 2 Explore</span>',
